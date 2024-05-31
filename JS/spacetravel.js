a1fb5b5311ccfce8c149b4422359ad40956f98e9
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-31 02:54:58 -0700

    Attempted to add modal onclick for tours in Gallery.

--- JS/spacetravel.js
+++ JS/spacetravel.js
@@ -1,48 +1,85 @@
-// THIS FILE WAS CREATED WITH HELP FROM:  w3schools.com
+// THIS FILE WAS CREATED WITH CODING HELP FROM:  w3schools.com
 
 
-// A function to open the Modal:
+// To open the modal:
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
+  document.getElementById("myModal").style.display = "block";
+}
+
+// To close the modal:
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+
+// Start the index of tours at 1:
+var tourIndex = 1;
+
+// Start the fullsizeImage function loops with n being equal to 1:
+fullsizeImage(tourIndex);
+
+// Let the modal know which number in the array of full-sized images to open.
+// The value of number is passed in within the HTML image tag's onclick instructions "currentSlide(1)", "currentSlide(2)", etc.
+function currentSlide(number) {
+  fullsizeImage(tourIndex = number);
+}
+
+
+
+// Create an array to hold all the possible tours:
+var tours = document.getElementsByClassName("ourTours");
+
+var dots = document.getElementsByClassName("scrollbarImage");
+
+
+// This shouldn't be needed but just in case...
+function plusSlides(n) {
+  fullsizeImage(tourIndex += n);
+}
+
+
+
+
+
+
+function fullsizeImage(n) {
+  var i;
+  tours = document.getElementsByClassName("ourTours");
+  dots = document.getElementsByClassName("scrollbarImage");
+  // var captionText = document.getElementByClassName("caption");
+  if (n > tours.length) { tourIndex = 1 }
+  if (n < 1) { tourIndex = tours.length }
+  for (i = 0; i < tours.length; i++) {
+   tours[i].style.display = "none";
   }
-  
-
-// A function to close the Modal:
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
+  tours[tourIndex - 1].style.display = "block";
+  dots[tourIndex - 1].className += " active";
+  // captionText.innerHTML = dots[tourIndex - 1].alt;
+}
 
 
-  // Adds the "Next" and "Previous" controls:
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
 
 
-  // Thumbnail image controls:
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-  
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("scrollbarImage");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+// // Get the image and insert it inside the modal
+// var img = document.getElementById("IronWolf");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
